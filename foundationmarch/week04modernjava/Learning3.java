ddf3b2ab283413e67cf90e1e74719ac49988b115
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2026-05-23 21:41:26 +0530

    throwAndThrows

--- foundationmarch/week04modernjava/Learning3.java
+++ foundationmarch/week04modernjava/Learning3.java
@@ -4,6 +4,11 @@ package foundationmarch.week04modernjava;
 public class Learning3 {
     public static void main(String[] args) {
         method();
+        try {
+            method2(10);
+        } catch (Exception e) {
+            System.out.println("Exception :" + e.getMessage());
+        }
     }
 
     
@@ -19,6 +24,18 @@ public class Learning3 {
         } finally{ 
             //code that alaways get executed, regardless of an exception occurs or not
             System.out.println("This will always get executed");
+        }
+    }
+
+    private static void method2(int age) throws Exception { 
+        //throws is used to declare that this method may throw an exceptiion,
+        // the coder who calls this method must handdle the exception, either with try-catch or by declaring throws in their method signature
+        if(age < 18) {
+            //throw is used to actually throw an exception when a certain condition is met
+            //message is optional, but it can provide more information about the exception
+            throw new Exception("Age must be 18 or above");
+        } else {
+            System.out.println("You are eligible to vote");
         }
     }
      

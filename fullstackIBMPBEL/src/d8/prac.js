40b58103982bbd10c4909cde015641b75f873bfd
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2026-08-13 11:22:41 +0530

    delete files

--- fullstackIBMPBEL/src/d8/prac.js
+++
@@ -1,9 +0,0 @@
-//string
-const str = "1,1,0,0,1,1,0,1,1,1,0,0";
-//string to arr using split
-const arr = str.split(",");
-//sorting method 
-// arr.sort();
-arr.sort((a,b) => b-a);
-
-console.log(arr);
